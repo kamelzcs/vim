954335f81d3b2d4fc6c369340f9108637e34477f
Zhao <zhao@luxeys.com>
2013-12-04 11:46:02 +0900

    fixed the exuberant-ctags problem

--- my_install.sh
+++ my_install.sh
@@ -2,5 +2,7 @@ ln -s ~/.vim_runtime/.vimrc ~/.vimrc
 ln -s ~/.vim_runtime/.vim ~/.vim
 ln -s ~/.vim_runtime/.dircolors ~/.dircolors
 source ~/.vimrc
+sudo apt-get install ctags
+sudo apt-get install exuberant-ctags
 eval `dircolors ~/.dircolors`
 ./set_dark.sh
